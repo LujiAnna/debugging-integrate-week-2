2bc07bb2ceab8b38830635e740904b719840589e
Anna <ikokianna@gmail.com>
2020-06-24 09:00:43 +0200

    errors, bugs, logic added

--- scripts/handlers/evens-or-odds.js
+++ scripts/handlers/evens-or-odds.js
@@ -4,30 +4,40 @@
 console.log('--- loading: evens or odds');
 
 function evensOrOddsHandler() {
-  debugger;
-  console.log('\n--- action: sum numbers in range ---');
-  console.log('range:', typeof range, '\n', range);
+    debugger;
+    console.log('\n--- action: sum numbers in range ---');
+    console.log('range:', typeof range, '\n', range);
 
-  // read user input
-  const displayEvens = confirm('would you like to see even numbers?');
-  console.log('displayEvens:', typeof displayEvens, '\n', displayEvens);
+    // read user input
+    const displayEvens = confirm('would you like to see even numbers?');
+    console.log('displayEvens:', typeof displayEvens, '\n', displayEvens);
 
-  // perform core logic
-  /* create a comma-separated list if all the evens or odds in the range
-    if range is 5 and the user asks for evens:
-      "2, 4, "
-    if range is 5 and the user asks for odds:
-      "1, 3, 5, "
-    if range is 8 and the user asks for evens:
-      "2, 4, 6, 8, "
-    ...
-  */
-  let listedNumbers = '';
-  for (_; _; _) {
+    // perform core logic
+    /* create a comma-separated list if all the evens or odds in the range
+      if range is 5 and the user asks for evens:
+        "2, 4, "
+      if range is 5 and the user asks for odds:
+        "1, 3, 5, "
+      if range is 8 and the user asks for evens:
+        "2, 4, 6, 8, "
+      ...
+    */
 
-  }
-  console.log('listedNumbers:', typeof listedNumbers, '\n', listedNumbers);
+    // Listing even numbers -- infinite loop!!!
+    let listedNumbers = '';
+    if (displayEvens === true) {
+        for (let i = 2; i <= range; i += 2) {
+            listedNumbers += `${i}, `;
+        }
+    }
 
-  // alert result for the user
-  alert(listedNumbers);
+    if (displayEvens === false) {
+        for (let i = 1; i <= range; i += 2) {
+            listedNumbers += `${i}, `;
+        }
+    }
+    console.log('listedNumbers:', typeof listedNumbers, '\n', listedNumbers);
+
+    // alert result for the user
+    alert(listedNumbers);
 }
